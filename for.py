6d87a80bdad0e7ff412fb92a8c2018263d10e43a
me865 <me865@MBP-di-me865.station>
2025-02-22 11:15:46 +0100

    Ulteriore commit

--- for.py
+++ for.py
@@ -1,4 +1,4 @@
-limite = int(input("Inserisci il numero fino al quale vuoi che conti: "))
+limite = int(input("Inserisci un numero fino al quale vuoi che conti: "))
 
 for i in range (1,limite+1,1):
     print(i)
